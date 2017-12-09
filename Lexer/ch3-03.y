36650686f315f8f7b3b879b7b91d6060449fa04d
Fahad Ausaf <fahad.ausaf@icloud.com>
2017-12-09 09:00:55 +0000

    Calculator grammar with variables and real values

--- Lexer/ch3-03.y
+++ Lexer/ch3-03.y
@@ -1,3 +1,5 @@
+
+
 %{
 #include <stdio.h>
 double vbltable[26];
--- Lexer/y.tab.h
+++ Lexer/y.tab.h
@@ -60,10 +60,10 @@ extern int yydebug;
 
 union YYSTYPE
 {
-#line 8 "ch3-04.y" /* yacc.c:1909  */
+#line 6 "ch3-03.y" /* yacc.c:1909  */
 
       double dval;
-      struct symtab *symp;
+      int vblno;
 
 #line 69 "y.tab.h" /* yacc.c:1909  */
 };
